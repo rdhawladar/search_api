d57aa7a012db1872f3b0bea5b9bfbc168ebb0c49
RD Hawladar <rdhawladar@gmail.com>
2019-12-28 20:08:50 +0600

    Test files for unit test added

---
+++ tests/Unit/RouteTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace tests\Unit;
+
+use TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * Route test -> home  url
+     *
+     * @return void
+     */
+    public function testHomeRoute()
+    {
+        $response = $this->get("/");
+        $this->assertEquals(200, $this->response->status());
+    }
+
+    /**
+     * Route test -> version 1 restaurants api route
+     *
+     * @return void
+     */
+    public function testRestaurantRouteV1()
+    {
+        $response = $this->get("/api/v1/restaurants");
+        $this->assertEquals(200, $this->response->status());
+    }
+
+    /**
+     * Route test -> version 5 restaurants api route
+     *
+     * @return void
+     */
+    public function testRestaurantRouteV5()
+    {
+        $response = $this->get("/api/v5/restaurants");
+        $this->assertEquals(200, $this->response->status());
+    }
+}
